9df6d425af7990d35baadaec7ef16545966db9c4
Denis Boaventura <denisgre07@gmail.com>
2020-08-22 15:53:36 -0300

    Incrementing the done status variable

--- src/data/yt_data_scrapping.py
+++ src/data/yt_data_scrapping.py
@@ -13,9 +13,9 @@ def getBSHtml(url):
     page_soup = soup(page_html, 'html.parser')
     return page_soup
 
-def getDate(url):
-    #get the date
-    page_soup = getBSHtml(url)
+def getDate(link):
+    #get the date and done status
+    page_soup = getBSHtml(link)
     date = re.findall(r'meta content="(.+?)".+?startDate"', page_soup.prettify())[0]
     date = re.split('[T+]', date)
     date = date[0] + ' ' + date[1]
@@ -25,29 +25,30 @@ def getDate(url):
     diff = timedelta(0,0,0,0,0,-3)
     fuse = timezone(diff)
     date = date.astimezone(fuse)
-    return date
+    now = localtimezone.localize(datetime.now(), is_dst=None)
+    done = now > date
+    return date, done
     
 def getDict(url):
-    #return a dictonary with the links, image links, date and the titles of the videos
-    link, date, data_list = [], [], []
-    data_frame = pd.DataFrame(columns=['img','date','title','link'])
+    #return a dictonary with the links, image links, date, titles of the videos and done status of the videos
+    data_frame = pd.DataFrame(columns=['img','date','title','link', 'done'])
     page_soup = getBSHtml(url)
     
     #find video id
     after_id = ',"playlistId":"PLa_Eeh4hhoGc_yIrmXWaWhnv5yLc9Ttab","index":'
-    id = re.findall(r'"videoId":"(.{,20})"'+after_id, page_soup.prettify())
+    video_ids = re.findall(r'"videoId":"(.{,20})"'+after_id, page_soup.prettify())
 
     #find video title 
-    title = re.findall(r'"title":.+?":"(.+?) Love Bits', page_soup.prettify())
+    titles = re.findall(r'"title":.+?":"(.+?) Love Bits', page_soup.prettify())
     
     #find video image link
-    img = re.findall(r'138},{"url":"(.+?)\?sqp.+?', page_soup.prettify())
+    imgs = re.findall(r'138},{"url":"(.+?)\?sqp.+?', page_soup.prettify())
         
     #find link and date, and create dictonary
-    for count, i in enumerate(id):
-        link.append('https://www.youtube.com/watch?v='+i)
-        date.append(getDate(link[count]))
-        data_frame.loc[count] = [img[count], date[count], title[count], link[count]] 
+    for count, video_id in enumerate(video_ids):
+        link = 'https://www.youtube.com/watch?v='+video_id
+        date, done = getDate(link)
+        data_frame.loc[count] = [imgs[count], date, titles[count], link, done] 
     
     #order data frame by date
     data_frame.sort_values(by='date', ascending=False, inplace=True)
@@ -56,12 +57,11 @@ def getDict(url):
     data_frame['date'] = data_frame['date'].apply(lambda date : re.split('[ :-]',str(date)))
     data_frame['date'] = data_frame['date'].apply(lambda date : date[2]+'/'+date[1]+'/'+date[0]+' às '+date[3]+':'+date[4]+' horas')
     data_list = data_frame.to_dict('records')
-    
     return {'meetings':data_list}
 
 
 url = 'https://www.youtube.com/playlist?list=PLa_Eeh4hhoGc_yIrmXWaWhnv5yLc9Ttab'
 data_dict = getDict(url)
-  
+
 with open('./src/data/meetings.json', 'w') as fp:
     json.dump(data_dict, fp)
